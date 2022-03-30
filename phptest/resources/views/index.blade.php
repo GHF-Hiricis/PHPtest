8f40a60336c7985c8881fecf9454464059d3c368
Gabriel Henrique Fachini <gabiel_henrique@hotmail.com>
2022-03-30 15:37:32 -0300

    create view index and app

--- phptest/resources/views/index.blade.php
+++ phptest/resources/views/index.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <h5 class="card-header">Olá quer consultar algum endereço?</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Este é um web site para consulta de endereços</h5>
+                    <p class="card-text">
+                        Digite o CEP que deseja consultar, e retornaremos o endereço
+                    </p>
+                    <a href="/adresses" class="btn btn-primary">Veja os CEPs já Consultados</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+
+            <form>
+                <div class="form-group">
+                    <label for="inputCEP">Digite o CEP que deseja consultar</label>
+                    <input type="text" class="form-control" id="inputCEP" placeholder="">
+                </div>
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+
+        </div>
+    </div>
 @endsection
